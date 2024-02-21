c26ee6b827ff73e2db198e0f3f9542f5da9fc407
Andre Santa Cruz <andre.santacruz@eoschools.org>
2024-02-21 11:11:15 -0500

    change yml config v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  reactStrictMode: true,
 };
-
-export default nextConfig;
+module.exports = nextConfig;
